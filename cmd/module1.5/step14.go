e493ced35250595ff4acf4a0274629c2ad26a3a5
Oleg Arefyev <arefyev.o.a@gmail.com>
2022-05-26 14:21:50 +0300

    add module 1.3 and 1.5

---
+++ cmd/module1.5/step14.go
@@ -0,0 +1,20 @@
+/*Дано натуральное число, выведите его последнюю цифру.
+Формат входных данных
+На вход дается натуральное число N, не превосходящее 10000.
+Формат выходных данных
+Выведите одно целое число - ответ на задачу.
+
+Sample Input:123
+Sample Output:3
+*/
+package main
+
+import "fmt"
+
+func main() {
+	var num string
+
+	_, _ = fmt.Scan(&num)
+	res := num[len(num)-1]
+	fmt.Println(string(res))
+}
